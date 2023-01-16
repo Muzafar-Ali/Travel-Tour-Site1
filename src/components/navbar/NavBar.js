62d1d9a0dd27370d4391a99131be6b4f1d3b003c
Muzafar-Ali <muzafar.ali.chandio@gmail.com>
2023-01-16 23:25:23 +0500

    add

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,10 +3,18 @@ import "./navbarStyles.css";
 import { MenuItems } from "./MenuItem";
 
 class NavBar extends Component{
+    state = {clicked: false}
+    handleClick = ()=>{
+        this.state({clicked : !this.state.clicked})
+    }
     render(){
         return(
             <nav className="NavBarItems">
                 <h1 className="navbar-logo">Trippy</h1>
+                <div className="menu-icons" onClick={this.handleClick}>
+                    {/* create toggle */}
+                    <i className={ this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
                 <ul className="nav-menu">
                     {
                         MenuItems.map((item,index)=>{
